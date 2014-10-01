3cad30d5139e0d553d07da5fd43f69ec1c01ec17
Tien Yuan <tien.yuan@gmail.com>
2014-10-01 22:18:22 +0000

    added task model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Users
+5.times do
+  user = User.new(
+    username:   Faker::Internet.user_name,
+    email:      Faker::Internet.email,
+    password:   Faker::Lorem.characters(10) 
+    )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+# Create Tasks
+15.times do
+  Task.create!(
+    description:  Faker::Lorem.paragraph,
+    user: users.sample
+  )
+end
+tasks = Task.all
+
+member = User.new(
+  username:     'member',
+  email:    'member@example.com', 
+  password: 'helloworld'
+)
+member.skip_confirmation!
+member.save!
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Task.count} tasks created"
