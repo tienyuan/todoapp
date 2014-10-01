5dad24a29f058b6b1a8d32dbf6eb445891d415f9
Tien Yuan <tien.yuan@gmail.com>
2014-10-01 17:26:49 +0000

    fixed problem with a setting and fixed devise to allow username

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,1 @@
+Todoapp::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
+    username 'testuser'
+    sequence(:email, 100) { |n| "person#{n}@example.com" }
+    password "helloworld"
+    password_confirmation "helloworld"
+    confirmed_at Time.now
   end
 end
