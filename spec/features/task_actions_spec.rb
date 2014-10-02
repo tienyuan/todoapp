df198a6dfd4050113da836477ace0db5a69028fe
Tien Yuan <tien.yuan@gmail.com>
2014-10-02 21:31:09 +0000

    turned the complete into a checkbox

--- spec/features/task_actions_spec.rb
+++ spec/features/task_actions_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "create task workflow" do
+describe "task workflows" do
 
   include Warden::Test::Helpers
   Warden.test_mode!
@@ -10,8 +10,8 @@ describe "create task workflow" do
     login_as(@user, :scope => :user)
   end
 
-  describe "new task" do
-    it "shows form" do
+  describe "sucessfully" do
+    it "creates a task" do
       visit tasks_path
       click_link "Create a New Todo"
       fill_in 'Description', with: "some task description here"
@@ -22,6 +22,17 @@ describe "create task workflow" do
       expect( page ).to have_content('Todo created!')
     end
   end
+
+  describe "sucessfully" do
+    it "creates a task" do
+      @task = create(:task, user: @user)
+      visit tasks_path
+      check "Completed"
+
+      expect( page ).to have_content('Todo completed!')
+    end
+  end
+
 
   after do
     Warden.test_reset!
