b65661ac9647344f0fb4da4db5c9212e6d1840ba
Tien Yuan <tien.yuan@gmail.com>
2014-10-04 16:17:53 +0000

    adding a nicer error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from Pundit::NotAuthorizedError do |exception|
-     redirect_to root_url, alert: exception.message
-   end
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def after_sign_in_path_for(resource)
     tasks_path
@@ -18,4 +16,15 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
   end
+
+  private
+
+  def user_not_authorized(exception)
+   policy_name = exception.policy.class.to_s.underscore
+
+   flash[:error] = I18n.t "pundit.#{policy_name}.#{exception.query}",
+     default: 'Please login.'
+   redirect_to(request.referrer || root_path)
+ end
+
 end
