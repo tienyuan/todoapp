518f8b5364f26a7938a654ee84cfb1944ac3e420
Tien Yuan <tien.yuan@gmail.com>
2014-10-04 18:26:49 +0000

    added a test for the scope

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,33 +2,40 @@ require 'rails_helper'
 
 describe Task do 
 
-  describe "time methods" do
+  describe "class methods" do
     before do
       @newtask = create(:task, created_at: Timecop.freeze(Time.now))
       @oldtask = create(:task, created_at: 7.days.ago)
+      @completedtask = create(:task, completed: true)
     end
 
-    describe 'calculates expiration date' do
+    describe '#expiration_date' do
       it "for the maximum" do
         expect( @newtask.expiration_date ).to eq(Time.now + 7.days)
       end
     end
 
-    describe 'calculates expiration date' do
+    describe '#expiration_date' do
       it "for the minimum" do
         expect( @oldtask.expiration_date ).to eq(Time.now)
       end
     end
 
-    describe 'calculates days remaining' do
+    describe '#days_remaining' do
       it "for the maximum" do
         expect( @newtask.days_remaining ).to eq(7)
       end
     end
 
-    describe 'calculates days remaining' do
+    describe '#days_remaining' do
       it "for the minimum" do
         expect( @oldtask.days_remaining ).to eq(0)
+      end
+    end
+    
+    describe '#hide_completed scope' do
+      it "does not include completed tasks" do
+        expect( Task.hide_completed ).not_to include(@completedtask)
       end
     end
 
