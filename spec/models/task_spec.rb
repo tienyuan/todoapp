053cb49c67c8c2277cacd8210029a482bed2a51e
Tien Yuan <tien.yuan@gmail.com>
2014-10-07 01:12:36 +0000

    reordered

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -50,8 +50,8 @@ describe Task do
 
     describe "ActiveRecord associations" do
       it { expect(@new_task).to belong_to(:user) }
-      it { expect(@user).to have_many(:tasks) }
       it { expect(@new_task).to have_db_index(:user_id) } 
+      it { expect(@user).to have_many(:tasks) }
     end
   end
 end
